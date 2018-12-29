f17a0fced9d62877c7f613a9849f06b35dc0a268
kayang0000 <18410422493@163.com>
2018-12-29 16:36:12 +0800

    optimize spring-timer

--- src/main/java/com/sojson/common/timer/ToTimer.java
+++ src/main/java/com/sojson/common/timer/ToTimer.java
@@ -1,37 +1,34 @@
 package com.sojson.common.timer;
 
-import java.util.Date;
-
-import javax.annotation.Resource;
-
+import com.sojson.permission.service.RoleService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.sojson.permission.service.RoleService;
+import javax.annotation.Resource;
+import java.util.Date;
 
 
 /**
  * 定时任务恢复数据
  *
+ * @author kqyang
+ * @version 1.0
+ * @date 2018/12/29 15:50
  */
 @Component
-public class ToTimer{
-	
-	@Resource
-	RoleService roleService;
-	@Scheduled(cron = "0/20 * * * * ? ")
-	public void run() {
-		/**
-		 * 调用存储过程，重新创建表，插入初始化数据。
-		 */
-		roleService.initData();
-		System.out.println(new Date().getTime());
-	}
+public class ToTimer {
 
-	
-	
-	
-	
-	
-	
+    @Resource
+    RoleService roleService;
+
+    @Scheduled(cron = "0/20 * * * * ? ")
+    public void run() {
+        /**
+         * 调用存储过程，重新创建表，插入初始化数据。
+         */
+        roleService.initData();
+        System.out.println(System.currentTimeMillis());
+    }
+
+
 }
